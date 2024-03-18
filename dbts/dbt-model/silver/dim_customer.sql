296db9f2b530e5ab03e52da4c86bd8af5c003341
wesdes <weslley.desouzabarboza@epic.com.mt>
2024-03-18 18:22:33 +0100

    dev silver layer

--- dbts/dbt-model/silver/dim_customer.sql
+++ dbts/dbt-model/silver/dim_customer.sql
@@ -1,28 +1,40 @@
-with raw as (
-    select * 
+with cust as (
+    select
+        cua.customer_id, 
+        cua.custcode, 
+        coa.co_id,  
+        coa.co_activated,
+        cua.customer_dealer,
+        cua.csactivated,
+        cca.ccfname,
+        cca.cclname
     from {{ ref('bscs_customer_all') }} as cua
     left join {{ ref('bscs_contract_all') }} as coa on cua.customer_id = coa.customer_id
     left join {{ ref('bscs_ccontact_all') }} as cca on cua.customer_id = cca.customer_id
     where 1=1
-    and cca.ccseq = 1
+    and cca.ccseq = '1'
 )
-, service as (
+, serv as (
     select
         co_id,
         cs_activ_date,
         cs_deactiv_date , 
-        rank() over (partition by co_id order by cs_deactiv_date desc)
+        rank() over (partition by co_id order by cs_deactiv_date desc) as rank
     from {{ ref('bscs_contr_services_cap') }}
 )
 select
-        rw.customer_id, 
-        rw.custcode, 
-        rw.co_id,  
-        rw.co_activated 
-        rw.c_activation_date, 
-        rw.customer_dealer, 
-        rw.csactivated
-    from raw as rw
-    left join service sr on rw.co_id and sr.co_id
+    cust.customer_id as natural_key
+    ,cust.custcode as business_key
+    ,cust.ccfname as first_name
+    ,cust.cclname as last_name
+    ,case when serv.cs_deactiv_date is null and serv.co_id is not null then 'Active' else 'Inactive' end as status
+    ,'Y' as registered_flg
+    ,null as registered_date
+    ,cust.csactivated as date_from
+    ,serv.cs_deactiv_date as c_date_to
+    ,now() as rec_creation
+	,now() as rec_update
+   from cust as cust
+   left join serv on cust.co_id = serv.co_id
 where 1=1
-and (sr.rank = 1 or sr.rank is null)
+and (serv.rank = 1 or serv.rank is null)

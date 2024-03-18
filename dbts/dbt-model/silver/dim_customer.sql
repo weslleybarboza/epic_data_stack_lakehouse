87ed615b7b9f47ddb947dd917e25231aa0447ca4
wesdes <weslley.desouzabarboza@epic.com.mt>
2024-03-18 19:51:57 +0100

    changed objects to type view

--- dbts/dbt-model/silver/dim_customer.sql
+++ dbts/dbt-model/silver/dim_customer.sql
@@ -8,9 +8,9 @@ with cust as (
         cua.csactivated,
         cca.ccfname,
         cca.cclname
-    from {{ ref('bscs_customer_all') }} as cua
-    left join {{ ref('bscs_contract_all') }} as coa on cua.customer_id = coa.customer_id
-    left join {{ ref('bscs_ccontact_all') }} as cca on cua.customer_id = cca.customer_id
+    from {{ ref('vw_bscs_customer_all') }} as cua
+    left join {{ ref('vw_bscs_contract_all') }} as coa on cua.customer_id = coa.customer_id
+    left join {{ ref('vw_bscs_ccontact_all') }} as cca on cua.customer_id = cca.customer_id
     where 1=1
     and cca.ccseq = '1'
 )
@@ -20,7 +20,7 @@ with cust as (
         cs_activ_date,
         cs_deactiv_date , 
         rank() over (partition by co_id order by cs_deactiv_date desc) as rank
-    from {{ ref('bscs_contr_services_cap') }}
+    from {{ ref('vw_bscs_contr_services_cap') }}
 )
 select
     cust.customer_id as natural_key
@@ -29,11 +29,11 @@ select
     ,cust.cclname as last_name
     ,case when serv.cs_deactiv_date is null and serv.co_id is not null then 'Active' else 'Inactive' end as status
     ,'Y' as registered_flg
-    ,null as registered_date
+    ,'' as registered_date
     ,cust.csactivated as date_from
     ,serv.cs_deactiv_date as c_date_to
-    ,now() as rec_creation
-	,now() as rec_update
+    ,current_date as rec_creation
+	,current_date as rec_update
    from cust as cust
    left join serv on cust.co_id = serv.co_id
 where 1=1

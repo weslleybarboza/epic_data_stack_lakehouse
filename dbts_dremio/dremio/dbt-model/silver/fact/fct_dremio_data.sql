7d2a5a57f3d312496aff84bcb7821a8c5b65dc19
wesdes <weslley.desouzabarboza@epic.com.mt>
2024-04-17 08:30:38 +0200

    added dbt project on Dremio

---
+++ dbts_dremio/dremio/dbt-model/silver/fact/fct_dremio_data.sql
@@ -0,0 +1,32 @@
+{{ config(
+    materialized = 'incremental',
+    incremental_strategy='merge',
+    unique_key = 'natural_key',
+    properties= {
+        "format": "'PARQUET'",
+        "partitioning": "ARRAY['rec_created']",
+        
+        }
+) }}
+
+with data_activity as (
+    select *
+    from {{ ref('stg_dremio_pscore_sgw') }}
+    
+    {% if is_incremental() %}
+    where rec_created >= (select date_add('day', -1, max(rec_created)) from {{ this }})
+    {% endif %}
+)
+select
+    da.charging_id               as natural_key
+    ,da.imsi                      as imsi_part_a
+    ,da.msisdn                   as msisdn_part_a
+    ,da.local_sequence_number    as id_dim_local
+    ,da.volume_uplink            as volume_upload
+    ,da.volume_downlink          as volume_download
+    ,da.total_volume             as volume_total
+    ,da.lac_or_tac               as id_dim_antena
+    ,da.mccmnc                   as id_dim_operator
+    ,da.output_filename          as cdr_file_name
+    ,da.rec_created
+from data_activity da

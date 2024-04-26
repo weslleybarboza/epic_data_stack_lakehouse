6a9a7eff7add8f3cfbd0c2d47af5d4a10bf75054
wesdes <weslley.desouzabarboza@epic.com.mt>
2024-04-26 15:38:07 +0200

    removed tools from stack

--- docker/jupyter/jupyter_server_config.py
+++
@@ -1,6 +0,0 @@
-c = get_config()
-c.ServerApp.ip = '0.0.0.0'
-c.ServerApp.port = 8899
-c.ServerApp.token = 'jupyter'
-c.ServerApp.password = ''
-c.ServerApp.terminado_settings = {'shell_command': ['/usr/bin/zsh']}
